fd054547255bc348f22b26f4ef4e1a9c139c84ac
fanghengwei <fanghengwei@gmail.com>
2019-03-24 09:46:15 +0800

    no message

---
+++ application/admin/lang/zh-cn/companyschoolblacklist.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'Name'  =>  'Name',
+    'Contact_name'  =>  'Contact_name',
+    'Contace_information'  =>  'Contace information',
+    'Reporter'  =>  'Reporter',
+    'Reson'  =>  'Reason for blacklist',
+    'Creater_id'  =>  'Recorder',
+    'Create_time'  =>  'Create time',
+    'Update_time'  =>  'Update time'
+];
---
+++ public/assets/js/backend/companyschoolblacklist.js
@@ -0,0 +1,57 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'companyschoolblacklist/index',
+                    add_url: 'companyschoolblacklist/add',
+                    edit_url: 'companyschoolblacklist/edit',
+                    del_url: 'companyschoolblacklist/del',
+                    multi_url: 'companyschoolblacklist/multi',
+                    table: 'company_school_black_list',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'name', title: __('Name')},
+                        {field: 'contact_name', title: __('Contact_name')},
+                        {field: 'contace_information', title: __('Contace_information')},
+                        {field: 'reporter', title: __('Reporter')},
+                        {field: 'reson', title: __('Reson')},
+                        {field: 'creater_id', title: __('Creater_id')},
+                        {field: 'create_time', title: __('Create_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'update_time', title: __('Update_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
