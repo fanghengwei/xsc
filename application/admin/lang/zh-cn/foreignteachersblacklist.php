a4df4829e9d97635b0b49d97e3ee9fb12e55a945
fanghengwei <1246166868@qq.com>
2019-09-07 15:20:58 +0800

    no message

--- application/admin/lang/zh-cn/foreignteachersblacklist.php
+++ application/admin/lang/zh-cn/foreignteachersblacklist.php
@@ -7,5 +7,6 @@ return [
     'Gender'  =>  'Gender',
     'Contact_information'  =>  'Contact information',
     'Reporter'  =>  'Reporter',
+    'Attachment_files'  =>  'Attachment',
     'Reason_for_blacklist'  =>  'Reason for blacklist'
 ];
