666168f310dc409919f89141f5e8a967373ae71e
fanghengwei <1246166868@qq.com>
2019-09-07 13:42:07 +0800

    no message

--- public/git.php
+++ public/git.php
@@ -1,6 +1,6 @@
 <?php
 echo '<pre>';
-print_r(exec("git -c diff.mnemonicprefix=false -c core.quotepath=false --no-optional-locks pull --no-commit origin master From https://github.com/fanghengwei/xsc"));
+print_r(exec("git -c diff.mnemonicprefix=false -c core.quotepath=false --no-optional-locks pull --no-commit origin master"));
 exit('</pre>');
 
 ?>
